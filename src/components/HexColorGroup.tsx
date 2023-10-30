cea503d98e920b38060744ed8c1411001e6c3045
scottchen98 <scott.chen1117@gmail.com>
2023-10-30 16:02:42 -0700

    make HexColorGroup component reusable

--- src/components/HexColorGroup.tsx
+++ src/components/HexColorGroup.tsx
@@ -1,20 +1,9 @@
 import HexColor from "./HexColor";
 
-export default function HexColorGroup() {
-  return (
-    <div className="grid grid-cols-3 gap-y-10">
-      <HexColor variant="primary" hexCode="#2563eb" colorName="Navy Blue" />
-      <HexColor variant="secondary" hexCode="#f1f5f9" colorName="Dove Grey" />
-      <HexColor
-        variant="foreground"
-        hexCode="#020817"
-        colorName="Midnight Black"
-      />
-      <HexColor
-        variant="destructive"
-        hexCode="#ef4444"
-        colorName="Danger Red"
-      />
-    </div>
-  );
+export default function HexColorGroup({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="grid grid-cols-3 gap-y-10">{children}</div>;
 }
--- src/components/MainPalette.tsx
+++ src/components/MainPalette.tsx
@@ -1,3 +1,4 @@
+import HexColor from "./HexColor";
 import HexColorGroup from "./HexColorGroup";
 
 export default function MainPalette() {
@@ -6,7 +7,20 @@ export default function MainPalette() {
       <h2 className="text-3xl font-semibold border border-y-0 border-r-0 border-l-8 pl-[10px] border-primary">
         Main Palette
       </h2>
-      <HexColorGroup />
+      <HexColorGroup>
+        <HexColor variant="primary" hexCode="#2563eb" colorName="Navy Blue" />
+        <HexColor variant="secondary" hexCode="#f1f5f9" colorName="Dove Grey" />
+        <HexColor
+          variant="foreground"
+          hexCode="#020817"
+          colorName="Midnight Black"
+        />
+        <HexColor
+          variant="destructive"
+          hexCode="#ef4444"
+          colorName="Danger Red"
+        />
+      </HexColorGroup>
     </div>
   );
 }
