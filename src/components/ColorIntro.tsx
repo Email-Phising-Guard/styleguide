8ce4791166cf373bf7317207a4153574e820ca05
scottchen98 <scott.chen1117@gmail.com>
2023-10-30 18:36:25 -0700

    add description

--- src/components/ColorIntro.tsx
+++ src/components/ColorIntro.tsx
@@ -2,10 +2,12 @@ export default function ColorIntro() {
   return (
     <div className="max-w-5xl mx-auto space-y-12 my-24">
       <h1 className="text-5xl font-bold tracking-wide">Colors</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum
-        voluptatum ea qui impedit nihil fuga, et vel quas cupiditate sit alias
-        placeat culpa beatae aliquid nostrum repellat optio quam atque!
+      <p className="text-2xl tracking-wider">
+        We chose this color palette for Email Phishing because the color scheme
+        develops a harmonious palette that conveys dependability and
+        professionalism (blue) with a dash of urgency and excitement (red). The
+        light grey keeps the overall look sleek and contemporary while acting as
+        a soft backdrop that lets the other colours pop.
       </p>
     </div>
   );
