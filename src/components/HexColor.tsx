5a4f6ba3535f6ab9e65e91fe251b3b7d9e6e9919
scottchen98 <scott.chen1117@gmail.com>
2023-10-30 18:37:04 -0700

    add colors to palette

--- src/components/HexColor.tsx
+++ src/components/HexColor.tsx
@@ -1,17 +1,31 @@
 export default function HexColor({
+  darkMode,
   variant,
   hexCode,
   colorName,
 }: {
+  darkMode: boolean;
   variant: string;
   hexCode: string;
   colorName: string;
 }) {
   return (
     <div className="mx-auto text-center">
-      <div className={`w-32 h-32 rounded-full bg-${variant}`}></div>
-      <p className="font-semibold tracking-wide mt-2">{hexCode}</p>
-      <p className="text-[10px]">{colorName}</p>
+      <div
+        className={`w-32 h-32 rounded-full ${
+          darkMode ? "dark" : ""
+        } bg-${variant}`}
+      ></div>
+      <p
+        className={`font-semibold tracking-wide mt-2 text-foreground ${
+          darkMode ? "dark" : ""
+        }`}
+      >
+        {hexCode}
+      </p>
+      <p className={`text-[10px] text-foreground ${darkMode ? "dark" : ""}`}>
+        {colorName}
+      </p>
     </div>
   );
 }
--- src/components/MainPalette.tsx
+++ src/components/MainPalette.tsx
@@ -7,20 +7,64 @@ export default function MainPalette() {
       <h2 className="text-3xl font-semibold border border-y-0 border-r-0 border-l-8 pl-[10px] border-primary">
         Main Palette
       </h2>
+      <h3 className="text-center text-xl font-bold">Light Mode</h3>
       <HexColorGroup>
-        <HexColor variant="primary" hexCode="#2563eb" colorName="Navy Blue" />
-        <HexColor variant="secondary" hexCode="#f1f5f9" colorName="Dove Grey" />
         <HexColor
+          darkMode={false}
+          variant="primary"
+          hexCode="#2563eb"
+          colorName="Navy Blue"
+        />
+        <HexColor
+          darkMode={false}
+          variant="secondary"
+          hexCode="#f1f5f9"
+          colorName="Dove Grey"
+        />
+        <HexColor
+          darkMode={false}
           variant="foreground"
           hexCode="#020817"
           colorName="Midnight Black"
         />
         <HexColor
+          darkMode={false}
           variant="destructive"
           hexCode="#ef4444"
           colorName="Danger Red"
         />
       </HexColorGroup>
+      <section className="dark bg-background space-y-10 py-8">
+        <h3 className="text-foreground text-center text-xl font-bold">
+          Dark Mode
+        </h3>
+        <HexColorGroup>
+          <HexColor
+            darkMode={true}
+            variant="primary"
+            hexCode="#3b82f6"
+            colorName="Navy Blue"
+          />
+          <HexColor
+            darkMode={true}
+            variant="secondary"
+            hexCode="#1e293b"
+            colorName="Dove Grey"
+          />
+          <HexColor
+            darkMode={true}
+            variant="foreground"
+            hexCode="#f8fafc"
+            colorName="Midnight Black"
+          />
+          <HexColor
+            darkMode={true}
+            variant="destructive"
+            hexCode="#7f1d1d"
+            colorName="Danger Red"
+          />
+        </HexColorGroup>
+      </section>
     </div>
   );
 }
